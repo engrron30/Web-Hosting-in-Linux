f8609ab3066819c88b0865794ae310bb0122fe31
engrron30 <ronaldtorresrosete30@gmail.com>
2025-06-10 14:01:10 +0800

    [MOD] Edit shell script for ngrok-v1 hosting

--- 03_host-web-using-ngrok/create-host.sh
+++ 03_host-web-using-ngrok/create-host.sh
@@ -7,8 +7,24 @@ echo "✅ Local IP is: $HOSTING_IP"
 echo "🚀 Starting local Python HTTP server on port $HOSTING_PT..."
 python3 -m http.server "$HOSTING_PT" &
 
-# Make sure you have already created your ngrok account to start
-# the web hosting. Run the command below after preparing your ngrok
-# account
+
+# (1) Make sure you have install ngrok. The command below can be used to install
+#     ngrok in your Ubuntu. 
+
+# curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+#  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+#  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+#  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+#  && sudo apt update \
+#  && sudo apt install ngrok
+
+# (2) After installing, add the ngrok token (which can be found in ngrok dashboard)
+#     with the following command:
+
+# ngrok config add-authtoken <token-id>
+
+# (3) Make sure you have already created your ngrok account to start
+#     the web hosting. Run the command below after preparing your ngrok
+#     account:
 
 # ngrok http http://localhost:8080
