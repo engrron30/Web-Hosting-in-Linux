401cc0c67798e6f9a89f035576d6d4d13608fcd8
engrron30 <ronaldtorresrosete30@gmail.com>
2025-06-10 13:20:49 +0800

    [MOD] Edit shell script for web hosting using ngrok

--- 03_host-web-using-ngrok/create-host.sh
+++ 03_host-web-using-ngrok/create-host.sh
@@ -6,3 +6,9 @@ HOSTING_PT="${2:-8080}"
 echo "✅ Local IP is: $HOSTING_IP"
 echo "🚀 Starting local Python HTTP server on port $HOSTING_PT..."
 python3 -m http.server "$HOSTING_PT" &
+
+# Make sure you have already created your ngrok account to start
+# the web hosting. Run the command below after preparing your ngrok
+# account
+
+# ngrok http http://localhost:8080
