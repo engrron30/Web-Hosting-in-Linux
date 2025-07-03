74ffc52a27078cad90fcfa20a8233d02e11254ca
Ronald Rosete <ronaldtorresrosete30@gmail.com>
2025-07-04 05:53:20 +0800

    Update create-web-on-local-ip.sh

--- 01_host-web-locally/create-web-on-local-ip.sh
+++ 01_host-web-locally/create-web-on-local-ip.sh
@@ -1,6 +1,6 @@
-sudo modprobe dummy
-sudo ip link add dummy0 type dummy
-sudo ip addr add 192.168.1.1/24 dev dummy0
-sudo ip link set dummy0 up
-sudo python3 -m http.server 8080
+modprobe dummy
+ip link add dummy0 type dummy
+ip addr add 192.168.1.1/24 dev dummy0
+ip link set dummy0 up
+python3 -m http.server 8080
 
